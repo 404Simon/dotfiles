75f6391dfe45411b65638db07fe7797da17f53ee
404Simon <i22038@hb.dhbw-stuttgart.de>
2025-06-20 21:16:53 +0200

    add dotfiles to tmux sessionizer

--- tmux-sessionizer.sh
+++ tmux-sessionizer.sh
@@ -3,7 +3,8 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~/dev ~/Vorlesungen -mindepth 1 -maxdepth 1 -type d | \
+    selected=$( (find ~/dev ~/Vorlesungen -mindepth 1 -maxdepth 1 -type d; \
+        echo "$HOME/dotfiles") | \
         sed "s|^$HOME/||" | \
         fzf --margin 10% --color="bw" 
     )
